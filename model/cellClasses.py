416eecd0e8add79fdec851b39a7069a24cf95fb1
Marianne Bezaire <mariannejcase@gmail.com>
2021-08-10 12:10:42 -0400

    Fix iclamp position

--- model/cellClasses.py
+++ model/cellClasses.py
@@ -26,16 +26,16 @@ class PyramidalCell(modelcell):
         super().__init__()
         self.gid = gid
         self.mgconc = mgconc
-        self.stimobj = h.IClamp(self.soma(0.5))
-        self.stimobj.delay = 1e9
-        self.stimobj.dur = 1
-        self.stimobj.amp = 0  
         self.list_syns = []
         self.create_sections() 
         self.build_topology()
         self.build_subsets() # subsets()
         self.define_geometry() # geom()
         self.define_biophysics() # biophys()
+        self.stimobj = h.IClamp(self.soma(0.5))
+        self.stimobj.delay = 1e9
+        self.stimobj.dur = 1
+        self.stimobj.amp = 0  
         # self.addSynapses() # synapses
         
     def __repr__(self):
